fdec1c213591bad46c4c795dfa2d88758930bba1
Md Ibrahim <106436211+10010891@users.noreply.github.com>
2022-06-21 23:06:58 +0600

    update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 var fileObjective = "this filw will be used only DOM related work"
 //================================================================//
+var update = "this update should be visible if everything goes alright"
