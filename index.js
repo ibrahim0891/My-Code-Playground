45a050f5203f16f8b00b95685f1a4a71185fc9fa
Md Ibrahim <106436211+10010891@users.noreply.github.com>
2022-06-21 23:14:44 +0600

    update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var fileObjective = "this filw will be used only DOM related work"
 //================================================================//
 var update = "this update should be visible if everything goes alright"
+this file has been updated to github successfulluy
