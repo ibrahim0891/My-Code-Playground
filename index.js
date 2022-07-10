cceaa9600496b0e62fb8436c33761660de39a6c0
nijhum0891@gmail.com <nijhum0891@gmail.com>
2022-07-10 13:47:46 +0600

    update index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ var fileObjective = "this file will be used only DOM related work"
 //================================================================//
 
 var upadate = "This text qill be visible if everything goes okay "
+console.log(upadate)
