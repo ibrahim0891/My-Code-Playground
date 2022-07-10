391d188aaadd2b40ffa62c3886e70ccc019ebe42
nijhum0891@gmail.com <nijhum0891@gmail.com>
2022-07-10 13:50:43 +0600

    hi

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ var fileObjective = "this file will be used only DOM related work"
 
 var upadate = "This text qill be visible if everything goes okay "
 console.log(upadate)
+
+console.log("hi github")
